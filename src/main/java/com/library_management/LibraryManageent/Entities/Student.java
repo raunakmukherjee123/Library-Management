1cd171dc3f2806685cc1f8a4c3fed728e89b897b
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2025-03-05 10:07:51 +0530

    Update Student.java

--- src/main/java/com/library_management/LibraryManageent/Entities/Student.java
+++ src/main/java/com/library_management/LibraryManageent/Entities/Student.java
@@ -6,12 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity//this annotation tells us that this class is the schema of database server
+@Entity
 @Table
-@Getter//develop all getter()
-@Setter//develop all setter()
-@NoArgsConstructor//default constructor
-@AllArgsConstructor//parameterized constructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,9 +24,3 @@ public class Student {
     @Column(unique = true,nullable = false)
      private String emailid;
 }
-//@getter,setter,NoArgsConstructor,AllArgsConstructor are internally created by lombok dependency. No need to create
-//getter(), setter(), constructor() manually
-//@Entity need default constructor mandatorily to work
-//by default, default constructor is always present in class, but
-//if we create parameterized constructor, default constructor is gone. So we manually create default constructor.
-//to change the name of table:      @Table(name="xyz")
